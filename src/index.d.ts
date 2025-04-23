817286e1cd73c4c0318038d2af80a02e37032d1d
Звард Геворгова <zvard.gevorgova@yandex.ru>
2025-04-23 20:33:41 +0300

    Задание types

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,18 +4,27 @@
 - age - число
 - gender - 'male' или 'female'
  */
-export function getPersons();
+export function getPersons(): Array<{
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+}>;
 
 /*
 Типизируйте функцию personToString, возвращающую строку и принимающую объект одного из следующих форматов:
-   1. Пользователь:
-   - name - строка
-   - age - число
-   - gender - 'male' или 'female'
-   2. Сотрудник
-   - name - строка
-   - age - число
-   - gender - 'male' или 'female'
-   - company- строка
- */
-export function personToString(person);
+ 1. Пользователь:
+ - name - строка
+ - age - число
+ - gender - 'male' или 'female'
+ 2. Сотрудник
+ - name - строка
+ - age - число
+ - gender - 'male' или 'female'
+ - company- строка
+*/
+export function personToString(person: {
+    name: string;
+    age: number;
+    gender: 'male' | 'female';
+    company?: string;
+}): string;
